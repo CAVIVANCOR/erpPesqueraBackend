6c96cffc173c4d9536b8029022f198d8a5f3289c
Carlos Alberto Vivanco Rodriguez <sistemas.cavr@gmail.com>
2026-04-15 18:32:23 -0500

    feat: agregar latitudFin y longitudFin a modelos Cala y CalaFaenaConsumo - 15042026

---
+++ prisma/migrations/20260415233034_latitudylongitudfin_cala150420261829/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Cala" ADD COLUMN     "latitudFin" DECIMAL(65,30),
+ADD COLUMN     "longitudFin" DECIMAL(65,30);
+
+-- AlterTable
+ALTER TABLE "CalaFaenaConsumo" ADD COLUMN     "latitudFin" DECIMAL(65,30),
+ADD COLUMN     "longitudFin" DECIMAL(65,30);
