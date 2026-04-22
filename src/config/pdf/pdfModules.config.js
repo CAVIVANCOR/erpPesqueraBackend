f4d14223f48c382d4aa080360a371538ea691767
Carlos Alberto Vivanco Rodriguez <sistemas.cavr@gmail.com>
2026-04-22 13:19:03 -0500

    refactorizando componentes pesca industrial y consumo

--- src/config/pdf/pdfModules.config.js
+++ src/config/pdf/pdfModules.config.js
@@ -598,7 +598,7 @@ const PDF_MODULES_CONFIG = {
     },
   },
 
-  "movimiento-caja-operacion": {
+   "movimiento-caja-operacion": {
     uploadPath: "uploads/pdf-system/movimiento-caja-operacion",
     oldPaths: [],
     apiEndpoint: "/api/pdf/movimiento-caja-operacion",
@@ -608,6 +608,32 @@ const PDF_MODULES_CONFIG = {
     database: {
       table: "MovimientoCaja",
       field: "urlComprobanteOperacionMovCaja", // ✅ CORREGIDO
+    },
+  },
+
+   "descarga-faena-pesca-comprobante-wincha": {
+    uploadPath: "uploads/pdf-system/descarga-faena-pesca-comprobante-wincha",
+    oldPaths: [],
+    apiEndpoint: "/api/pdf/descarga-faena-pesca-comprobante-wincha",
+    maxFileSize: 10 * 1024 * 1024,
+    allowedTypes: ["application/pdf", "image/jpeg", "image/png"],
+    maxFiles: 20,
+    database: {
+      table: "DescargaFaenaPesca",
+      field: "urlComprobanteWincha",
+    },
+  },
+
+  "descarga-faena-consumo-comprobante-wincha": {
+    uploadPath: "uploads/pdf-system/descarga-faena-consumo-comprobante-wincha",
+    oldPaths: [],
+    apiEndpoint: "/api/pdf/descarga-faena-consumo-comprobante-wincha",
+    maxFileSize: 10 * 1024 * 1024,
+    allowedTypes: ["application/pdf", "image/jpeg", "image/png"],
+    maxFiles: 20,
+    database: {
+      table: "DescargaFaenaConsumo",
+      field: "urlComprobanteWincha",
     },
   },
 };
