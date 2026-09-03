2706306f1300b0bb5e8d2bddba40b4b49dc5937d
Carlos Alberto Vivanco Rodriguez <sistemas.cavr@gmail.com>
2026-09-03 16:32:14 -0500

    feat: Corregir lógica clasificación tributaria Exportación vs Exonerado en Registro Ventas SUNAT

---
+++ prisma/migrations/20260903212957_agregarcamposenempresaparaelsiresunat030920261629/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Empresa" ADD COLUMN     "sunatClaveSol" TEXT,
+ADD COLUMN     "sunatClientId" TEXT,
+ADD COLUMN     "sunatClientSecret" TEXT,
+ADD COLUMN     "sunatUsuarioSol" TEXT;
